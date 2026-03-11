6f7c512b908e89c7dafad51b0ab12995ac5895e7
KucingUnyu14 <84355575+KucingUnyu14@users.noreply.github.com>
2026-03-11 22:29:47 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function order(){
+
+let id=document.getElementById("idgame").value
+let server=document.getElementById("server").value
+let produk=document.getElementById("produk").value
+
+let text=`Halo admin NexotopupStore saya mau top up
+
+ID Game: ${id}
+Server: ${server}
+Produk: ${produk}`
+
+window.open("https://wa.me/6281234567890?text="+encodeURIComponent(text))
+
+}
